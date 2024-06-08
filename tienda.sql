20e092b4c3f44ed71ec91041b1d2332f95febda2
Alex Reyes <73624939+AlexReyesHuezo@users.noreply.github.com>
2024-06-07 21:06:38 -0600

    change to english for team convention and refactor the sales table

--- tienda.sql
+++ tienda.sql
@@ -1,21 +1,30 @@
-CREATE DATABASE tienda;
+CREATE DATABASE store;
 
-USE tienda;
+USE store;
 
-CREATE TABLE productos(
+CREATE TABLE products (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(50) NOT NULL,
-    descripcion VARCHAR(100) NOT NULL,
-    precio DECIMAL(10, 2) NOT NULL,
-    talla VARCHAR(10) NOT NULL,
+    name VARCHAR(25) NOT NULL,
+    description VARCHAR(40) NOT NULL,
+    -- DECIMAL(5, 2) means that the number will have a maximum of 10 digits, 2 of them will be decimals
+    price DECIMAL(5, 2) NOT NULL,
+    size VARCHAR(10) NOT NULL,
     color VARCHAR(15),
-    cantidadStock INT
+    stock_quantity INT
 );
 
-CREATE TABLE ventas(
+CREATE TABLE sales (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    cantidad INT NOT NULL,
-    -- La fecha de la venta se guardará automaticamente con la fecha y hora actual
-    -- CURRENT_TIMESTAMP es una funcion de MySQL que guarda la fecha y hora actual
-    fecha DATE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    /*
+      Sales date will be saved automatically with the current date and time and the format YYYY-MM-DD HH:MM:SS
+      This is done with the CURRENT_TIMESTAMP MySQL function and the TIMESTAMP data type
+      I don't need to insert a value for this column, it will be done automatically
+      I don't use the date data type because it doesn't have the time part
+    */
+    sale_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    client_name VARCHAR(25) NOT NULL,
+    product VARCHAR(25) NOT NULL,
+    price DECIMAL(5, 2) NOT NULL,
+    FOREIGN KEY (product) REFERENCES products(name)
+    FOREIGN KEY (client_name) REFERENCES clients(name)
 );
