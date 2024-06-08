b167f80d2810d4c131d20250f6128bf2b9c1d2e4
Alex Reyes <73624939+AlexReyesHuezo@users.noreply.github.com>
2024-06-07 18:04:09 -0600

    use db, create productos and ventas tables

--- tienda.sql
+++ tienda.sql
@@ -1,1 +1,21 @@
 CREATE DATABASE tienda;
+
+USE tiendita;
+
+CREATE TABLE Productos(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    descripcion VARCHAR(100) NOT NULL,
+    precio DECIMAL(10, 2) NOT NULL,
+    talla VARCHAR(10) NOT NULL,
+    color VARCHAR(15),
+    cantidadStock INT
+);
+
+CREATE TABLE Ventas(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    cantidad INT NOT NULL,
+    -- La fecha de la venta se guardará automaticamente con la fecha y hora actual --
+    -- CURRENT_TIMESTAMP es una funcion de MySQL que guarda la fecha y hora actual --
+    fecha DATE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+);
